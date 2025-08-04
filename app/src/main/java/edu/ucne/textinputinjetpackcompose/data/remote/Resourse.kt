a34bde264d600ce62f81ef9454da7837d88d74a6
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-08-04 05:06:06 -0400

    Resourse creado

--- app/src/main/java/edu/ucne/textinputinjetpackcompose/data/remote/Resourse.kt
+++ app/src/main/java/edu/ucne/textinputinjetpackcompose/data/remote/Resourse.kt
@@ -1,4 +1,7 @@
 package edu.ucne.textinputinjetpackcompose.data.remote
 
-class Resourse {
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Error<T>(message: String, data: T? = null ): Resource<T>(data,message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
 }
