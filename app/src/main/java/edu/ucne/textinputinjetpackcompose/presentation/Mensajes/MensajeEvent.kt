c3432ad31895c4cc0f0e41d008a6f4bbc9625639
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-08-04 05:10:16 -0400

    Eventos de mensajes creados

---
+++ app/src/main/java/edu/ucne/textinputinjetpackcompose/presentation/Mensajes/MensajeEvent.kt
@@ -0,0 +1,16 @@
+package edu.ucne.textinputinjetpackcompose.presentation.Mensajes
+
+import java.util.Date
+
+sealed interface MensajeEvent {
+    data class MensajeChange(val mensajeId:Int): MensajeEvent
+    data class FechaChange(val fecha: Date): MensajeEvent
+    data class ContenidoChange(val contenido: String): MensajeEvent
+    data class RemitenteChange(val remitente: String): MensajeEvent
+    data class TipoRemitenteChange(val tipoRemitente: String): MensajeEvent
+
+    data object Save : MensajeEvent
+    data object Delete: MensajeEvent
+    data object New: MensajeEvent
+}
+
