162e24ede8ed5e66292c73cb9ad46b5a96b5735f
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-08-04 05:01:54 -0400

    Automatizando OTP Code por SMS

---
+++ app/src/main/java/edu/ucne/textinputinjetpackcompose/presentation/twoFactorAuthentications/AutomaticallyFilingScreen.kt
@@ -0,0 +1,139 @@
+package edu.ucne.textinputinjetpackcompose.presentation.twoFactorAuthentications
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.maxLength
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.ContentType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.contentType
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.ucne.textinputinjetpackcompose.R
+
+@Composable
+fun AutomaticallyFillingScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.secondary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_pet_app_2),
+                    contentDescription = "App Logo",
+                    modifier = Modifier.size(200.dp)
+                )
+
+                Text(
+                    text = "Pet Adoption",
+                    style = MaterialTheme.typography.headlineLarge.copy(
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    ),
+                )
+            }
+
+            Card(
+                shape = RoundedCornerShape(24.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.onSecondary),
+                elevation = CardDefaults.cardElevation(8.dp),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(text = "Enter OTP Code")
+
+                    val otpState = rememberTextFieldState()
+                    BasicTextField(
+                        state = otpState,
+                        modifier = Modifier.semantics(){
+                            contentType = ContentType.SmsOtpCode
+                        }.fillMaxWidth(),
+                        inputTransformation = InputTransformation.maxLength(6),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        ),
+                        lineLimits = TextFieldLineLimits.SingleLine,
+
+                        decorator = {
+                            val otpCode = otpState.text.toString()
+                            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                                repeat(6) { index ->
+                                    Digit(otpCode.getOrElse(index, {' '}  ))
+                                }
+                            }
+                        }
+                    )
+                }
+
+                TextButton(
+                    onClick = { },
+                    Modifier.align(Alignment.End)
+
+                ) {
+                    Text(
+                        text = "Resend code",
+                        color = MaterialTheme.colorScheme.secondary,
+                        fontWeight = FontWeight.Bold,
+                        textDecoration = TextDecoration.Underline
+
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .height(56.dp),
+                shape = RoundedCornerShape(24.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.tertiary
+                )
+            ) {
+                Text(
+                    text = "Submit",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
