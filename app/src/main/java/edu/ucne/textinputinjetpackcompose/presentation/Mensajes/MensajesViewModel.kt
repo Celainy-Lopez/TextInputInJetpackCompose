5d069794cfdbe31711f7e7623967a36a3c1f0471
Celainy-Lopez <celainy_lopez@ucne.edu.do>
2025-08-04 05:12:17 -0400

    MensajesViewModel creado

---
+++ app/src/main/java/edu/ucne/textinputinjetpackcompose/presentation/Mensajes/MensajesViewModel.kt
@@ -0,0 +1,121 @@
+package edu.ucne.textinputinjetpackcompose.presentation.Mensajes
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.textinputinjetpackcompose.data.local.entities.MensajeEntity
+import edu.ucne.textinputinjetpackcompose.data.repository.MensajesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.util.Date
+import javax.inject.Inject
+import kotlin.String
+
+@HiltViewModel
+class MensajesViewModel @Inject constructor(
+    private val mensajesRepository: MensajesRepository
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow(MensajeUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onEvent(event: MensajeEvent){
+        when(event) {
+            is MensajeEvent.MensajeChange -> onMensajeIdChange(event.mensajeId)
+            is MensajeEvent.FechaChange -> onFechaChange(event.fecha)
+            is MensajeEvent.ContenidoChange -> onContenidoChange(event.contenido)
+            is MensajeEvent.RemitenteChange -> onRemitenteChange(event.remitente)
+            is MensajeEvent.TipoRemitenteChange -> onTipoRemitenteChange(event.tipoRemitente)
+
+            MensajeEvent.Save -> saveMensaje()
+            MensajeEvent.New -> nuevo()
+            MensajeEvent.Delete -> deleteMensaje()
+        }
+    }
+
+    private fun saveMensaje() {
+        viewModelScope.launch {
+            if (_uiState.value.contenido.isNullOrBlank()){
+                _uiState.update {
+                    it.copy(errorMessage = "Campo vacio!!!")
+                }
+            }
+            else{
+                mensajesRepository.save(_uiState.value.toEntity())
+                _uiState.update {
+                    it.copy(contenido = "")
+                }
+            }
+        }
+    }
+
+
+    private fun nuevo(){
+        _uiState.update {
+            it.copy(
+                mensajeId = null,
+                fecha = Date(),
+                contenido = "",
+                remitente = "",
+            )
+        }
+    }
+
+    fun getMensajes() {
+        viewModelScope.launch {
+            mensajesRepository.getAll().collect { mensajes ->
+                _uiState.update {
+                    it.copy(mensajes = mensajes)
+                }
+            }
+        }
+    }
+
+
+    private fun deleteMensaje() {
+        viewModelScope.launch {
+            mensajesRepository.delete(_uiState.value.toEntity())
+        }
+    }
+
+
+    private fun onFechaChange(fecha: Date) {
+        _uiState.update {
+            it.copy(fecha = fecha)
+        }
+    }
+
+    private fun onMensajeIdChange(id: Int) {
+        _uiState.update {
+            it.copy(mensajeId = id)
+        }
+    }
+
+    private fun onContenidoChange(contenido: String){
+        _uiState.update {
+            it.copy(contenido = contenido)
+        }
+    }
+
+    private fun onRemitenteChange(remitente: String){
+        _uiState.update {
+            it.copy(remitente = remitente)
+        }
+    }
+
+    private fun onTipoRemitenteChange(tipoRemitente: String){
+        _uiState.update {
+            it.copy(tipoRemitente = tipoRemitente)
+        }
+    }
+}
+
+fun MensajeUiState.toEntity() = MensajeEntity(
+    mensajeId = mensajeId,
+    fecha = fecha ?: Date(),
+    contenido = contenido ?: "",
+    remitente = remitente ?: "",
+    tipoRemitente = tipoRemitente ?: "",
+)
