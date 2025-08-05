969ca8ba01097091994d426ce39a6ad0e977f877
Yohualkis <Yohualkis99@gmail.com>
2025-08-05 00:50:33 -0400

    Se refactorizo el codigo para arreglar el texto visble dentro del collar

--- app/src/main/java/edu/ucne/textinputinjetpackcompose/presentation/collarCustomizationScreen/CollarScreen.kt
+++ app/src/main/java/edu/ucne/textinputinjetpackcompose/presentation/collarCustomizationScreen/CollarScreen.kt
@@ -1,52 +1,55 @@
 package edu.ucne.textinputinjetpackcompose.presentation.collarCustomizationScreen
 
-import edu.ucne.textinputinjetpackcompose.presentation.twoFactorAuthentications.Digit
+import android.R.attr.stepSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.maxLength
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.autofill.ContentType
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.contentType
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.ucne.textinputinjetpackcompose.R
+import kotlin.math.max
+import kotlin.math.min
 
 @Composable
-fun CollarScren() {
-
+fun CollarScreen(
+    modifier: Modifier,
+) {
     val collarNameState = rememberTextFieldState()
     val collarPhoneState = rememberTextFieldState()
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.secondary),
+            .background(MaterialTheme.colorScheme.secondary)
+            .verticalScroll(rememberScrollState()),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -79,38 +82,57 @@ fun CollarScren() {
                     )
                     .padding(12.dp)
             ) {
+
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
                         .background(
                             color = MaterialTheme.colorScheme.tertiary,
                             shape = RoundedCornerShape(180.dp)
-                        ),
+                        )
+                        .padding(16.dp), // Margen interno para el texto
                     contentAlignment = Alignment.Center
                 ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    )  {
-                        Text(
-                            text = collarNameState.text.toString(),
-                            fontWeight = FontWeight.ExtraBold,
-                            style = MaterialTheme.typography.headlineLarge
-                        )
-                        Spacer(modifier = Modifier.height(4.dp))
+                    var currentFontSize by remember { mutableStateOf(72.sp) }
+                    val minFontSize = 32.sp
+                    val maxFontSize = 112.sp
+                    val stepSize = 0.005.sp
+                    Text(
+                        text = collarNameState.text.toString() + "\n${collarPhoneState.text}",
+                        fontWeight = FontWeight.ExtraBold,
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                            .padding(8.dp),
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineLarge.copy(
+                            lineHeight = currentFontSize * 1.01f
+                        ),
+                        overflow = TextOverflow.Ellipsis,
+                        autoSize = TextAutoSize.StepBased(
+                            minFontSize = 32.sp,
+                            maxFontSize = 72.sp,
+                        ),
+                        maxLines = 3,
+                        softWrap = true,
+                        onTextLayout = { textLayoutResult ->
+                            val currentSizeValue = currentFontSize.value // Obtenemos el valor Float
+                            val stepValue = stepSize.value
 
-                        Text(
-                            text = collarPhoneState.text.toString(),
-                            fontWeight = FontWeight.ExtraBold,
-                            style = MaterialTheme.typography.headlineLarge
-                        )
-                    }
+                            if (textLayoutResult.didOverflowHeight || textLayoutResult.didOverflowWidth) {
+                                // Reducir (operando con Floats)
+                                currentFontSize =
+                                    (currentSizeValue - stepValue).coerceAtLeast(minFontSize.value).sp
+                            } else if (currentSizeValue < maxFontSize.value) {
+                                // Aumentar (operando con Floats)
+                                currentFontSize =
+                                    (currentSizeValue + stepValue).coerceAtMost(maxFontSize.value).sp
+                            }
+                        }
+                    )
                 }
             }
 
-
             Spacer(modifier = Modifier.height(24.dp))
-
 
             Card(
                 shape = RoundedCornerShape(24.dp),
@@ -125,6 +147,7 @@ fun CollarScren() {
                 ) {
                     OutlinedTextField(
                         state = collarNameState,
+                        inputTransformation = InputTransformation.maxLength(16),
                         lineLimits = TextFieldLineLimits.SingleLine,
                         placeholder = { Text("CollarName") },
                         modifier = Modifier
@@ -140,6 +163,8 @@ fun CollarScren() {
 
                     OutlinedTextField(
                         state = collarPhoneState,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                        inputTransformation = InputTransformation.maxLength(10),
                         modifier = Modifier.fillMaxWidth(),
                         placeholder = { Text("CollarPhone") },
                         lineLimits = TextFieldLineLimits.SingleLine,
@@ -175,4 +200,10 @@ fun CollarScren() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    CollarScreen(Modifier)
 }
